592d282e2292151700eb1750f223cdda3a82b3c1
HenryOrsm <97636138+HenryOrsm@users.noreply.github.com>
2024-02-07 14:10:11 +1000

    start game
    rivers cuomom do you like my game

--- Objects/Title.py
+++ Objects/Title.py
@@ -1,4 +1,5 @@
 from GameFrame import RoomObject
+import pygame
 
 class Title(RoomObject):
     """
@@ -11,3 +12,13 @@ class Title(RoomObject):
         image = self.load_image("Title.png")
         self.set_image(image,800,350)
         
+        # register for key events
+        self.handle_key_events = True 
+
+    def key_pressed(self, key):
+        """
+        If the key pressed is space the game will start
+        """
+        
+        if key[pygame.K_SPACE]:
+            self.room.running = False
